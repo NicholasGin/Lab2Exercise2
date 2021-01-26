848ad5e1314b008eb1d1a6ef3f0446efd8fd023f
NicholasGin <nicholasgin@gmail.com>
2021-01-26 02:50:12 -0500

    renamed files
    added a label to the scene builder
    
    SampleHandlerController
    label changes to random location after hovering

--- src/SampleHandler/Main.java
+++ src/SampleHandler/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("SampleHandler.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("SampleHandler");
+        primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
 
--- src/SampleHandler/SampleHandlerController.java
+++ src/SampleHandler/SampleHandlerController.java
@@ -1,4 +1,15 @@
 package SampleHandler;
+import java.util.Random;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class SampleHandlerController {
+    @FXML
+    private Label jumpingLabel;
+
+    public void movinglabel(){
+        Random randomPos = new Random();
+        jumpingLabel.setLayoutX(randomPos.nextInt(500));
+        jumpingLabel.setLayoutY(randomPos.nextInt(300));
+    }
 }
